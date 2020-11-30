45a8129f1c63cdad175cfbcad6787e76d7b6a434
Yusradahir <32307311+Yusradahir@users.noreply.github.com>
2020-11-30 18:39:29 +0300

    Finished level 1
    All tests are now passing.

--- 01_Classes/src/main/java/bootcamp/kata/process/Processor.java
+++ 01_Classes/src/main/java/bootcamp/kata/process/Processor.java
@@ -6,8 +6,13 @@ package bootcamp.kata.process;
 import bootcamp.kata.data.Params;
 import bootcamp.kata.data.Results;
 
+import java.math.RoundingMode;
+
+
 public class Processor {
     public Results process(final Params params) {
-        return null; // FIXME
+
+
+        return new Results(params.getX().add( params.getY()),params.getX().subtract(params.getY()), params.getX().multiply(params.getY()), params.getX().divide(params.getY()));
     }
 }
